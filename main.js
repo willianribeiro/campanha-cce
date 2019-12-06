251695c56bed622442c55b5dc65955f0f8d0c8dc
Willian Ribeiro <willian.ribeiro@buser.com.br>
2019-12-06 18:43:10 -0300

    Add campo parcelas e mensagem por tipo de pagamento

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 window.onload = function () {
   const $form = $('form#js-form')
-  const $msgSuccess = $('#js-sucesso')
+  const $msgSuccess = $('#js-msg-sucesso')
   const $btnSubmit = $('#js-form-submit')
+  const $contribItau = $('#js-via-itau')
+  const $contribBB = $('#js-via-bb')
+  const $contribSantander = $('#js-via-santander')
+  const $contribCartao = $('#js-via-cartao')
+  const $contribBoleto = $('#js-via-boleto')
   const url = 'https://script.google.com/macros/s/AKfycbxRCsDwB88yfzA-J0EXC4ge3AbCnztFnehxVzGvnYzDaN8bPqo/exec'
 
   $btnSubmit.on('click', function(e) {
@@ -15,6 +20,28 @@ window.onload = function () {
       .done(function () {
         $form.hide()
         $msgSuccess.show()
+        const formaPagamento = $form.find('select[name="forma_pagamento"]').val()
+
+        if (formaPagamento === 'itau') {
+          $contribItau.show()
+        }
+
+        if (formaPagamento === 'brasil') {
+          $contribBB.show()
+        }
+
+        if (formaPagamento === 'santander') {
+          $contribSantander.show()
+        }
+
+        if (formaPagamento === 'cartao') {
+          $contribCartao.show()
+        }
+
+        if (formaPagamento === 'boleto') {
+          $contribBoleto.show()
+        }
+
       })
   })
 }
